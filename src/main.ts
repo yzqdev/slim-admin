facd5c80bc20f94855de891fa38002bb9db874e8
yzqdev <yzqdev@outlook.com>
2022-05-12 17:38:32 +0800

    add tailwind

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import MdEditorV3 from "md-editor-v3";
 import "md-editor-v3/lib/style.css";
+import "./index.css";
 import { pinia } from "@/store";
 import axios from "axios";
+
 import * as util from "util";
 import { setupDirectives } from "@/directives";
 const app = createApp(App);
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    textColor: {
+      primary: "#3490dc",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
